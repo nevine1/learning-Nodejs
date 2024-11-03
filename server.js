3f20cdee27fdfce302e65ef139618516389e5a93
nevine1 <venasamaan@gmail.com>
2024-11-02 21:08:44 -0400

    start learning Nodejs

---
+++ server.js
@@ -0,0 +1,16 @@
+//import os from 'os'
+//const math = require('./math');
+const { add, subtract, multiply, divide } = require('./math');
+const os = require('os')
+/* console.log(os.type());
+console.log(os.homedir());
+console.log(os.version()) */
+/* console.log(__dirname)
+console.log(__filename)
+console.log(os.platform()) */
+
+//console.log(math)
+//console.log(math.add(10, 4))
+//console.log(math.subtract(10, 4))
+console.log(subtract(10, 4))
+console.log(multiply(3, 5))
