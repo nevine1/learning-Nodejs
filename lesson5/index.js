6ba4ba5983ee102d4f8beff59fae0745ff385553
nevine1 <venasamaan@gmail.com>
2024-11-27 20:29:15 -0500

    using express-validtor and updating course using course id

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-
+const { body, validationResult } = require('express-validator')
 app.use(express.json());
 
 const courses = [
@@ -15,28 +15,63 @@ const courses = [
         price: 400
     }
 ]
-app.get("/api/courses", (req, res) => { //localhost:4000/api/courses
-    res.json(courses);
-})
 
 app.get("/api/courses/:id", (req, res) => { 
-    const id = parseInt(req.params.id); // Convert `id` to a number
+    const id = parseInt(req.params.id); 
     const course = courses.find((course) => course.id === id);
 
     if (!course) {
-        return res.status(404).json({ error: "Course not found" }); // Send a 404 response
+        return res.status(404).json({ error: "Course not found" }); 
     }
 
-    res.json(course); // Send the course data only if it exists
+    res.json(course); 
 });
+app.post("/api/courses", 
+    body('title')
+        .notEmpty()
+        .isLength({min: 5})
+        .withMessage('Course title must be at least 5 char'),
+    body('price')
+        .notEmpty(),
+(req, res) => { 
 
+    const errors = validationResult(req, res);
+    if(!errors.isEmpty()){
+        return res.status(400).json({error: errors.array()})
+    }
+    const { title, price } = req.body; 
+    const newCourse = {id: courses.length + 1, ...req.body};
+    courses.push(newCourse)
+   
+    res.json(newCourse)//return only the new course has been created
+   // res.json(courses); //return all the courses 
+})
+
+app.patch("/api/courses/:id", (req, res) =>{
+    const courseId = parseInt(req.params.id) //parseInt to convert id to integer 
+    const course = courses.find((course) => course.id === courseId);
+
+    if(!course){
+        return res.status(400).json("course not found")
+    }
+    const updatedCourse = {...req.body};
+    console.log(updatedCourse);
+    res.json(updatedCourse);
+
+})
+
+app.post("/api/courses", (req, res)=>{
+
+})
 //create new course 
 app.post("/api/courses", (req, res) =>{
     console.log(req.body);//the result is undefined , so middleware should be added (app.use(express.json())) 
     const { title, price } = req.body; 
-    courses.push({id : courses.length + 1, ...req.body});
+    const newCourse ={id : courses.length + 1, ...req.body}
+    courses.push(newCourse);
 
-    res.json(courses);
+    //res.status(200).statusMessage("new course created successfully").json(courses);
+    res.json(courses)
 })
 
 app.listen(4000, () =>{
